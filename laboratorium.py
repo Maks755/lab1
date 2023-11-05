af6a8b4da0e86c07f2a0ff4fac511bd3c6800460
Maks755 <149810675+Maks755@users.noreply.github.com>
2023-11-05 21:12:24 +0100

    Update laboratorium.py

--- laboratorium.py
+++ laboratorium.py
@@ -1,5 +1,5 @@
-y = int(input("podaj liczbę boku: "))
-z = int(input("podaj liczbę podstawy: "))
+y = int(input("podaj długość boku: "))
+z = int(input("podaj długość podstawy: "))
 p = y*z
 o = 2*y + 2*z
 print("pole prostokąta o podanych wymiarach to " + str(int(p)) + "cm2, a obwód to "+str(int(o)) + "cm")
